b0e6e5efd53bfb350281a958a11baafee95fd453
Alexander Gapak <mirmiclub@gmail.com>
2021-12-28 22:50:05 +0000

    update wallet.sh

--- contract/wallet.sh
+++ contract/wallet.sh
@@ -1,24 +1,25 @@
+#!/bin/bash
+
 RESULT_DIR="generated"
 FC_FILE="highload-wallet-v2-code.fc"
+WALLET_FIF_FILE="highload-wallet-v2-code.fif"
+NEW_WALLET_FIFT_FILE="new-highload-wallet-v2.fif"
+
+fiftPath="/usr/bin/ton/crypto/fift"
+smartcontPath="/usr/src/ton/crypto/smartcont"
+
+export FIFTPATH="/usr/src/ton/crypto/fift/lib:${smartcontPath}"
 
 mkdir $RESULT_DIR
-cp $FC_FILE $RESULT_DIR
 
-FULL_DIR="`pwd`/$RESULT_DIR"
+/usr/bin/ton/crypto/func \
+ -SPA "${smartcontPath}/stdlib.fc" ${FC_FILE} -o ${RESULT_DIR}/${WALLET_FIF_FILE}
 
-/usr/bin/ton/compile.sh $FULL_DIR 0
-rm $RESULT_DIR/$FC_FILE
-
-FIFT_FILE="new-highload-wallet-v2.fif"
-FIFT_PATH="/usr/bin/ton/crypto/fift"
-
-cp $FIFT_FILE $RESULT_DIR
-
+cp $NEW_WALLET_FIFT_FILE $RESULT_DIR
 cd $RESULT_DIR
 
-${FIFT_PATH} -s ${FIFT_FILE} 0 1 >> res.txt
-rm ${FIFT_FILE}
-rm "highload-wallet-v2-code.fif"
+${fiftPath} -s ${NEW_WALLET_FIFT_FILE} 0 1 >> res.txt
+rm ${NEW_WALLET_FIFT_FILE} ${WALLET_FIF_FILE}
 
 echo "Highload wallet address: 
 
@@ -26,10 +27,10 @@ echo "Highload wallet address:
 -- Bounceable:     `awk '/Bounceable address/{print $NF}' res.txt`
 -- Raw:            `awk '/new wallet address/{print $NF}' res.txt`
 
-Some more info:
+More info:
 
 -- Init query_id is:  `awk '/Init query_id/{print $NF}' res.txt`
 -- Signing message:   `awk '/signing message/{print $NF}' res.txt`
-" >> h.txt
+" >> wallet-info.txt
 
 rm res.txt
