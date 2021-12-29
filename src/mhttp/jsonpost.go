d8a04535188104bcc5f44d4895765bbfafca667f
Alexander Gapak <mirmiclub@gmail.com>
2021-12-29 19:07:36 +0000

    add copyright

--- src/mhttp/jsonpost.go
+++ src/mhttp/jsonpost.go
@@ -1,3 +1,24 @@
+/*
+highload-wallet-api – API wrapper over high-load TON wallet smart contract
+
+Copyright (C) 2021 Alexander Gapak
+
+This file is part of highload-wallet-api.
+
+highload-wallet-api is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+highload-wallet-api is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with highload-wallet-api.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package mhttp
 
 import (
